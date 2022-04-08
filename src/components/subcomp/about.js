1f36e47526860e1d26a27ea1b456b709825bc31c
princeraghul <raghulraghu2002@gmail.com>
2022-04-08 17:11:18 +0530

    second commit

--- src/components/subcomp/about.js
+++ src/components/subcomp/about.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import apartment from '../../styles/apartment.css';
 
 function About() {
   return (
-    <div>About</div>
+   <>
+     <div className='head'>
+   <h1 className='head1'>About us</h1> 
+   <h1 className='head2'>About us</h1>
+   </div>
+   <p className='text-muted' style={{margin:'50px' }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Founded in 1959 by a visionary entrepreneur,<mark> Mr Raghul Real Estates </mark>is an eminent real estate conglomerate and one of the leading builders in Chennai. We are constantly expanding our footprint with exquisite dwellings and elevated lifestyle solutions in our apartments in Chennai . Our clientele spans thousands of happy residents and families in this beloved city.
+
+From residences that can be appreciated by the burgeoning upper middle class to extravagant luxury homes in Chennai that are akin to castles in the air, we’ve done it all over our 60 years in the real estate industry. Our Founder, Mr Raghul is widely credited to have introduced the “Apartment Culture” in Chennai by being one of the first to offer elegantly designed residences. Additionally, we were the builders in Chennai to pioneer the concept of creating townships in key suburban locations through the urban development model – one that is now replicated by many others.
+
+Our vision is to identify and develop properties in Chennai that inspire and integrate value creation, impeccable quality, modernity and excellence. We aim to continue fostering an everlasting bond of customer relationship built on trust, confidence and delight while adhering to principles of ethics and professionalism.
+
+In the early ‘90s, Real Estates forayed into the hospitality sector. The Residency Group of Hotels, strategically located in Chennai, Coimbatore, Karur, and Pondicherry established new standards for business hotels in South India. In 2016, the hospitality division made its International debut in the Maldives, with the opening of an uber-luxury resort boasting of avant-garde designs under the highly prestigious St. REGIS brand.
+
+Over the last decade, our Founder's son & Chairman Mr. Ravi Appasamy has admirably chartered a steady growth plan for expansion of both the real estate & hospitality divisions. Now, with Rohit Ravi(son of Ravi Appasamy) also on the Board as our Managing Director, the company is all set to scale even greater heights in the coming decade under this dynamic and spirited leadership. Under their leadership, we’ve diversified our inventory to cover affordable apartments such as our Splendour in Semmenchery OMR and The Bloomingdale in Pammal as well as the luxury residences such as our Trellis and Capella in Vadapalani and Clover By The River in Kotturpuram. We even have our own brand of commercial spaces called Superette. Currently, they are located right outside Splendour and The Bloomingdale ensuring our homeowners in OMR and Pammal can enjoy a luxurious shopping experience right at their doorstep.
+
+Over the past couple of years, we’ve also experimented with creating properties for new market segments. As a result, we launched our ultra-luxury residences at Azure The Oceanic at R A Puram in Chennai that guarantees endless ocean views and utterly blissful life at the heart of this bustling city.
+
+The property also cemented a long-term collaboration between the brand and the Singapore-based WOW architects, known across the globe for their daring design and thoughtfully crafted spaces. This collaboration also extends to Altezza, our brand new flats for sale in Chennai. Unlike the other properties, Altezza is ’s attempt at incorporating and introducing sustainable construction to Chennai’s real estate market.</p>
+   </>
   )
 }
 
--- src/components/subcomp/disclaimer.js
+++ src/components/subcomp/disclaimer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import apartment from '../../styles/apartment.css';
 
 function Disclaimer() {
   return (
-    <div>Disclaimer</div>
+   <> 
+    <div className='head'>
+   <h1 className='head1'>Disclaimer</h1> 
+   <h1 className='head2'>Disclaimer</h1>
+   </div>
+  <p className='text-muted' style={{margin:'50px' }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This website is the sole property of <mark>Real Estates Limited.</mark> (the use of “AREL” hereinafter, implies  Real Estates Limited). The information contained in this site is for prospective purchasers and general guidance on matters of interest only. AREL provides information based on sources believed to be accurate and reliable. Users are responsible for seeking the advice of professionals, as appropriate, regarding the information, specifications, features, opinions, FAQs, advice or content available at AREL. AREL makes no representations and disclaims all warranties, whether express, implied, or statutory, including but not limited to warranties as to accuracy, reliability, usefulness, completeness, or fitness of information for any particular purpose, non-infringement and any damages ensuing thereby. In no event shall AREL or its Chairman, Managing Director, directors, managers, officers, employees or content providers be liable for any direct, indirect, special, incidental, exemplary, punitive or consequential damages, whether or not advised of the possibility of such damages including without limitation, those pertaining to lost profits to any AREL subscriber, participant, customer, or other person or entity for furnishing of information or arising from the contents/use of this website. </p>
+  </>
   )
 }
 
